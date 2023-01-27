7ee8eeb9454fa27006984cf15f47580396e6b6a1
jaiswalk008 <jaiskaran008@gmail.com>
2023-01-27 16:27:26 +0530

    third assignment

--- script.js
+++ script.js
@@ -31,23 +31,23 @@ method for html collections*/
 
 // get elements by class name
 
-// let items = document.getElementsByClassName('list-group-item');
+let items = document.getElementsByClassName('list-group-item');
 // console.log(items);
 // console.log(items[0]);
 // console.log(items[2]);
-// items[1].textContent='hello';
+items[1].textContent='hello';
 // items[1].style.fontWeight = 'bold';
 // items[1].style.backgroundColor = 'yellow';
-// //wont work because its a html collection
-// // items.style.backgroundColor='red';
-// for(let i=0;i<items.length;i++){
-//     items[i].style.backgroundColor='red';
-// }
+//wont work because its a html collection
+// items.style.backgroundColor='red';
+for(let i=0;i<items.length;i++){
+    items[i].style.backgroundColor='red';
+}
 
 //get elemts by tag name
 
 let li = document.getElementsByTagName('li');
-// console.log(typeof li);
+ console.log(li);
 // console.log(li); //selects all the elements by tag name of 'li'
 li[2].style.backgroundColor = 'green';
 for(let i=0;i<li.length;i++){
